04d04e256388b4691e77e3a5c732a493e258cd42
Roma <romanofkin@gmail.com>
2018-04-28 19:07:45 -0700

    Regex with sed

--- Basics/1.6_regex.sh
+++ Basics/1.6_regex.sh
@@ -16,7 +16,7 @@ read -p "Validate Date (yyyy/mm/dd) : " date
 #--------------------------------------
 # Regular Expression: validate with pattern
 # Example: 2018/04/28 or 2018-04-28
-#--------------------------------------
+#-----------------------------------
 pattern="^[0-9]{4}[/|-][0-9]{2}[/|-][0-9]{2}$"
 
 if [[ $date =~ $pattern ]]; then
@@ -24,3 +24,15 @@ if [[ $date =~ $pattern ]]; then
 else
   echo "$date is NOT valid"
 fi
+
+
+#--------------------------------------
+# Format the date entered into a standard form
+# Pipe $date into sed to substitute all the formats into one standard form with -
+# Example: April 28 2018 converted to April-28-2018
+#--------------------------------------
+
+read -p "Enter a date: " date
+
+clean_date=$( echo $date | sed 's/[ /:\^#]/-/g' )
+echo $clean_date
